aa7f6edc7e2e278d55921f01c4d77645f532c26a
Andreas Olofsson <ao@blazar.se>
2017-02-27 12:19:15 +0100

    Merged js files for the kitchen page into one file and fixed some css.
    * Merged 'public/js/kitchenVue.js' and 'public/js/kitchen.js' into
        'public/js/kitchen.js' and fixed onLoad for Vue.
    * Changed css for 'views/kitchen.html' to have consistent width when
        items are added and removed.

--- public/js/kitchen.js
+++ public/js/kitchen.js
@@ -1,15 +1,91 @@
-/* global sharedVueStuff, Vue, socket */
 'use strict';
 
-new Vue({
-  el: '#orders',
-  mixins: [sharedVueStuff], // include stuff that goes to both diner and kitchen
-  methods: {
-    markDone: function(orderid) {
-      this.orders[orderid].done = true;
-      socket.emit("orderDone", orderid);
-    }
-  }
-});
+var vue;
 
-// ******************************************************
+// ***************************
+function kitchenPageLoaded() { // functions specific to the index.html document
+    displayItems();
+
+	vue = new Vue({
+		el: '#knapp',
+		methods: {
+			send: function() {
+				sendOrder();
+			},
+			ready: function() {
+				changeRdy();
+			}
+		}
+	});
+}
+
+// ***************************
+function displayItems() {
+    displayButton('knapp', "Send order from bar");
+}
+
+// **********
+function displayButton(id, txt) {
+    var button  = document.getElementById(id);
+    button.append(txt);
+}
+
+window.addEventListener("load", kitchenPageLoaded);
+
+// **************************
+var table = 1;
+
+function Order(id, table, foods) {
+	this.id = id;
+	this.table = table;
+	this.foods = foods;
+}
+
+function Food(name, count, specials) {
+	this.name = name;
+	this.count = count || 1;
+	this.specials = specials;
+}
+// ***************************
+function sendOrder() {
+    var order = ["Table " + table, "2, Burger", "1, Soup"];
+    displayNewOrder(order);
+	table++;
+}
+
+function displayNewOrder(order) {
+    var pointer = document.getElementById('ongoing');
+    var div     = document.createElement('div');
+    div.setAttribute('class', 'orderClass');
+    div.append(order[0]); // the TABLE#
+    div.append(createButton());
+    for(var i=1; i < order.length; i++) {
+        div.append(order[i]); // the FOOD
+    }
+    pointer.appendChild(div);
+}
+
+
+
+// ***************************
+function changeRdy(e) {
+    console.log("*** changeRdy ***");
+    var newParent = document.getElementById('completed');
+    var oldParent = document.getElementById('ongoing');
+
+	var btn = e.target;
+	newParent.appendChild(btn.parentElement);
+}
+
+// ***************************
+function createButton() {
+    var button= document.createElement('button');
+    button.setAttribute('type', 'submit');
+    button.setAttribute('class', 'rdyButton');
+    button.setAttribute('name', 'rdyButton');
+	button.setAttribute('id', "btnid:" + table);
+    button.addEventListener('click', changeRdy);
+    // button.setAttribute('v-on:click', changeRdy);
+    button.append("ready");
+    return button;
+}
